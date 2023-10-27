5a372939d6f104d3a400a3e75bca44333b642e9a
Belén <belen.munoz@usach.cl>
2023-10-26 22:53:52 -0300

    feat: add super admin y acceso admin DRF

--- hrsapp/admin.py
+++ hrsapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from hrsapp.models.paciente import Paciente
+from hrsapp.models.gestor import Gestor
+from hrsapp.models.diagnostico import Diagnostico
+
+admin.site.register(Paciente)
+admin.site.register(Gestor)
+admin.site.register(Diagnostico)
