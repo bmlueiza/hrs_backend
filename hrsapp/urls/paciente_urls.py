17de45aada7ac92e729e36ad4b2aebcbde826fe7
Belén <belen.munoz@usach.cl>
2023-10-29 23:01:50 -0300

    refactor: change forma de tratar las urls

--- hrsapp/urls/paciente_urls.py
+++ hrsapp/urls/paciente_urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from hrsapp.views.paciente_views import ListaPacientes
+import hrsapp.views.paciente_views as Pacienteviews
 
 urlpatterns = [
-    path("api/pacientes/", ListaPacientes.as_view(), name="lista_pacientes"),
-    # path('api/pacientes/<int:pk>/', views.DetallePaciente.as_view(), name='detalle_paciente'),
+    path(
+        "api/pacientes/",
+        Pacienteviews.PacienteListView.as_view(),
+        name="lista_pacientes",
+    ),
+    path(
+        "api/pacientes/<int:pk>/",
+        Pacienteviews.PacienteDetailView.as_view(),
+        name="detalle_paciente",
+    ),
     # Otras rutas relacionadas con pacientes
 ]
