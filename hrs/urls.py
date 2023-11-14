6ecb5d63ecc48612a4f648b2f1890c7348659f39
Belén <belen.munoz@usach.cl>
2023-11-14 13:03:07 -0300

    feat: fix add url de Core API schema

--- hrs/urls.py
+++ hrs/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("docs/", include_docs_urls(title="HRS API Documentation")),
     # Rutas de pacientes desde la app hrsapp
     path("hrsapp/", include("hrsapp.urls.paciente_urls")),
     # Rutas de recomendaciones desde la app hrsapp
