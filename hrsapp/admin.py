7aefb5997ccb3e11be5424ed12d7be12ebb4a15d
Belén <belen.munoz@usach.cl>
2023-11-28 01:50:03 -0300

    refactor: actualización requirements.txt y registro de modelos en admin
    - Actualización del documento requirements.txt
    - Registro de modelos para el uso del administrador en Django.

--- hrsapp/admin.py
+++ hrsapp/admin.py
@@ -1,24 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
-from hrsapp.models.paciente import Paciente
-from hrsapp.models.gestor import Gestor
 from hrsapp.models.diagnostico import Diagnostico
+from hrsapp.models.gestor import Gestor
+from hrsapp.models.paciente import Paciente
+from hrsapp.models.medico import Medico
 from hrsapp.models.medicamento import Medicamento
-from hrsapp.models.seguimiento_paciente import SeguimientoPaciente
+from hrsapp.models.actividad_medica import ActividadMedica
+from hrsapp.models.accion_gestor import AccionGestor
+from hrsapp.models.resultado_contacto import ResultadoContacto
+from hrsapp.models.historial_contacto import HistorialContacto
 from hrsapp.models.recomendacion import Recomendacion
 from hrsapp.models.observacion import Observacion
-from hrsapp.models.medico import Medico
-from hrsapp.models.control import Control
-from hrsapp.models.asignacion_medicamento import AsignacionMedicamento
+from hrsapp.models.asignacion_actividad import AsignacionActividad
+from hrsapp.models.historial_medicamento import HistorialMedicamento
 
-admin.site.register(Paciente)
-admin.site.register(Gestor)
 admin.site.register(Diagnostico)
+admin.site.register(Gestor)
+admin.site.register(Paciente)
+admin.site.register(Medico)
 admin.site.register(Medicamento)
-admin.site.register(SeguimientoPaciente)
+admin.site.register(ActividadMedica)
+admin.site.register(AccionGestor)
+admin.site.register(ResultadoContacto)
+admin.site.register(HistorialContacto)
 admin.site.register(Recomendacion)
 admin.site.register(Observacion)
-admin.site.register(Medico)
-admin.site.register(Control)
-admin.site.register(AsignacionMedicamento)
+admin.site.register(AsignacionActividad)
+admin.site.register(HistorialMedicamento)
