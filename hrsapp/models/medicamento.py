af4f0dab1b7217b7af3c67ac3a3d0b2e0a1be0a3
Belén <belen.munoz@usach.cl>
2023-12-18 23:21:54 -0300

    refactor: eliminacion de código de medicamento

--- hrsapp/models/medicamento.py
+++ hrsapp/models/medicamento.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class Medicamento(models.Model):
-    nombre = models.CharField(max_length=50)
-    codigo = models.CharField(max_length=10, blank=True, null=True)
+    nombre = models.CharField(unique=True, max_length=50)
     descripcion = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
@@ -11,6 +10,4 @@ class Medicamento(models.Model):
 
     @classmethod
     def buscar_medicamentos(cls, query):
-        return cls.objects.filter(
-            models.Q(nombre__icontains=query) | models.Q(codigo__icontains=query)
-        )
+        return cls.objects.filter(models.Q(nombre__icontains=query))
--- hrsapp/serializers/medicamento_serializer.py
+++ hrsapp/serializers/medicamento_serializer.py
@@ -7,15 +7,6 @@ class MedicamentoSerializer(serializers.ModelSerializer):
         model = Medicamento
         fields = "__all__"
 
-    def validate_codigo(self, value):
-        if value == "":
-            raise serializers.ValidationError("Debe ingresar un código.")
-        elif Medicamento.objects.filter(codigo=value).exists():
-            raise serializers.ValidationError(
-                "Ya existe un medicamento con ese código."
-            )
-        return value
-
     def create(self, validated_data):
         medicamento = Medicamento.objects.create(**validated_data)
         return medicamento
