791c26621afccbd902b389cf07a0566c46d0aed6
Belén <belen.munoz@usach.cl>
2023-12-31 23:53:20 -0300

    fix: especialidad como doble atributo en medico.py

--- hrsapp/models/medico.py
+++ hrsapp/models/medico.py
@@ -6,7 +6,6 @@ class Medico(models.Model):
     rut = models.CharField(max_length=12, unique=True)
     nombre = models.CharField(max_length=25)
     apellido = models.CharField(max_length=25)
-    especialidad = models.CharField(max_length=50)
 
     # Foreign Keys
     especialidad = models.ForeignKey(
